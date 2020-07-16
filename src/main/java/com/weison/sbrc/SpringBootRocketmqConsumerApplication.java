11b2127404a7a5237ac12d0c8bb95978fa280a3d
weixiaoxing <weixiaoxing@meishubao.com>
2020-07-16 13:07:59 +0800

    first commit

---
+++ src/main/java/com/weison/sbrc/SpringBootRocketmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.weison.sbrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRocketmqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRocketmqConsumerApplication.class, args);
+    }
+
+}
